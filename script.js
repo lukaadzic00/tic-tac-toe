3f10509f105cbc3b463fd54af39018dfbecb5559
unknown <luka.adzic.rs@gmail.com>
2025-05-22 20:38:12 +0200

    Zavrsen celokupan projekat. Ostaje prostor za moduliranje koda

--- script.js
+++ script.js
@@ -2,22 +2,47 @@ const Modul = (function () {
     const gameboard = [["", "", ""],
                         ["", "", ""],
                         ["", "", ""]];
-    let winner;
-    let turn = 1;
     let player1, player2;
+    let move = 0;
     let gameActive = true;
     const btnStart = document.querySelector('#start');
+    const btnRestart = document.querySelector('#restart');
+    const fields = document.querySelectorAll("#gameboard div");
+    const divDisplay = document.querySelector('#result');
 
     function getUserInput(str) {
         return document.querySelector(str).value;
     }
     
-    function createPlayer(name, sign) {
-        return {name, sign};
+    function createPlayer(name, sign, turn) {
+        return {name, sign, turn};
+    }
+
+    function restartGame() {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                gameboard[i][j] = "";
+            }
+        }
+        move = 0;
+        gameActive = true;
+
+        fields.forEach((field) => {
+            field.textContent = "";
+        });
+
+        divDisplay.textContent = "";
+    }
+
+    function displayResult(winner) {        
+        if(winner) {
+            divDisplay.textContent = "The winner is: " +  winner.name;
+        } else {
+            divDisplay.textContent = "It's a draw!";
+        }
     }
 
     function setEventsAndIndexesOnAllDivs() {
-        const fields = document.querySelectorAll("#gameboard div");
         let index = 0;
 
         fields.forEach((field) => {
@@ -25,27 +50,41 @@ const Modul = (function () {
             field.dataset.col = index % 3;
             index++;
 
-            field.addEventListener('click', (e) => {markerField(e.target)});
+            field.addEventListener('click', (e) => {markField(e.target)});
         });
     }
 
-    function markerField(field) {
-        if(!gameActive) {
+    function markField(field) {
+        if(!gameActive || field.textContent !== "") {
             return;
         }
-        const currentPlayer = (turn === 1) ? player1 : (turn === 2 ? player2 : null);
+
+        let currentPlayer, nextPlayer;
+        if(player1.turn) {
+            currentPlayer = player1;
+            nextPlayer = player2;
+        } else if(player2.turn) {
+            currentPlayer = player2;
+            nextPlayer = player1;
+        }
 
         gameboard[field.dataset.row][field.dataset.col] = currentPlayer.sign;
         field.textContent = currentPlayer.sign;
+        move++;
 
-        if(checkForWinner(currentPlayer)) {
-            winner = currentPlayer;
-            console.log("GAME OVER, Winner is: " + currentPlayer.name);
+        if(checkForWinner(currentPlayer, move)) {
+            displayResult(currentPlayer);
+            gameActive = false;
+            return;
+        }
+        if(move >= 9) {
+            displayResult();
             gameActive = false;
             return;
         }
 
-        turn = (turn === 1) ? 2 : 1;
+        currentPlayer.turn = false;
+        nextPlayer.turn = true;
     }
 
     function checkForWinner(currentPlayer) {
@@ -70,14 +109,20 @@ const Modul = (function () {
         if(gameboard[0][2] === sign && gameboard[1][1] === sign && gameboard[2][0] === sign) {
             return true;
         }
+        return false;
     }    
 
     function startGame() {
-        player1 = createPlayer(getUserInput('#player1'), 'X');
-        player2 = createPlayer(getUserInput('#player2'), 'O');
+        player1 = createPlayer(getUserInput('#player1'), 'X', true);
+        player2 = createPlayer(getUserInput('#player2'), 'O', false);
 
+        if(player1.name.trim() === "" || player2.name.trim() === "") {
+            alert("Please enter names for both players");
+            return;
+        }
         setEventsAndIndexesOnAllDivs()
     }
 
     btnStart.addEventListener('click', startGame);
+    btnRestart.addEventListener('click', restartGame);
 })();
